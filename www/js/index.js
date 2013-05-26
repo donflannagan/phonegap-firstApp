ff855a39a39a5d8ac5336b3027953cb65b1f9651
Don Flannagan <don.flannagan@gmail.com>
2013-05-25 22:51:50 -0700

    Updated to implement detail page

--- www/js/index.js
+++ www/js/index.js
@@ -6,10 +6,30 @@ function loadRepos() {
     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
         var i, repo;
         $.each(data.repositories, function (i, repo) {
-            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            $("#allRepos").append("<li><a href='repo-detail.html?owner=" + repo.username + "&name=" + repo.name + "'>"
             + "<h4>" + repo.name + "</h4>"
             + "<p>" + repo.username + "</p></a></li>");
         });
         $('#allRepos').listview('refresh');
     });
+}
+
+$('#reposDetail').live('pageshow', function(event) {
+    var owner = getUrlVars().owner;
+    var name = getUrlVars().name;
+    loadRepoDetail(owner,name);
+});
+
+function loadRepoDetail(owner,name) {
+     $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
+         var repo = data;
+         console.log(data);
+
+         $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
+         $('#description').text(repo.description);
+         $('#forks').html("<strong>Forks:</strong> " + repo.forks + "<br><strong>Watchers:</strong> " + repo.watchers);
+
+         $('#avatar').attr('src', repo.owner.avatar_url);
+         $('#ownerName').html("<strong>Owner:</strong> <a href='" + repo.owner.url + "'>" + repo.owner.login + "</a>");
+     });
 }
